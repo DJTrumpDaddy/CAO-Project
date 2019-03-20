294d56f719d9eeacb9f98da92d10efd7b4e6c083
Logan Wood <logan-wood@uiowa.edu>
2019-03-20 10:51:06 -0700

    modified ctrl outputs and cleaning up sisc module

--- P2/sisc.v
+++ P2/sisc.v
@@ -3,18 +3,18 @@
 
 `timescale 1ns/100ps  
 
-module sisc (clk, rst_f, ir);
+module sisc (clk, rst_f);
 
   input clk, rst_f;
-  input [31:0] ir;
-
-// declare all internal wires here
-  wire[1:0] alu_op; //alu_op 
-  wire wb_sel; //wb_sel
-  wire rf_we; //rf_we
-  wire stat_en, br_sel, pc_sel; //stat_en
   
-  wire[3:0] read_regb; //read_regb
+// declare all internal wires here
+  wire[1:0] alu_op; //ctrl.v output
+  wire rf_we, wb_sel, br_sel, pc_sel, ir_load; //ctrl.v outputs
+  wire stat_en;
+  
+  wire [31:0] instr; //ir.v outputs
+
+  wire[3:0] mux4_out; //mux4.v outputs
   wire[31:0] rsa; //rsa
   wire[31:0] rsb; //rsb
   wire[3:0] stat_in; //stat_in
@@ -27,13 +27,13 @@ module sisc (clk, rst_f, ir);
 
 // component instantiation goes here
 
-  ctrl ControlUnit (clk, rst_f, ir[31:28] /*opcode*/, ir[27:24] /*mm*/, stat_out, rf_we, alu_op, wb_sel, br_sel, pc_sel);
+  ctrl ControlUnit (clk, rst_f, instr[31:28] /*opcode*/, iinstr[27:24] /*mm*/, stat_out, rf_we, alu_op, wb_sel, br_sel, pc_sel, ir_load);
   
-  mux4 Mux4 (ir[15:12] /*in_a*/, ir[23:20] /*in_b*/, 1'b0, read_regb);
+  mux4 Mux4 (instr[15:12] /*in_a*/, instr[23:20] /*in_b*/, 1'b0, mux4_out);
   
-  rf RegisterFile (clk, ir[19:16] /*read_rega*/, read_regb, ir[23:20] /*write_reg*/, write_data, rf_we, rsa, rsb);
+  rf RegisterFile (clk, instr[19:16] /*read_rega*/, mux4_out, instr[23:20] /*write_reg*/, write_data, rf_we, rsa, rsb);
   
-  alu ArithmeticLogicUnit(clk, rsa, rsb, ir[15:0], alu_op, alu_result, stat_in, stat_en);
+  alu ArithmeticLogicUnit(clk, rsa, rsb, instr[15:0], alu_op, alu_result, stat_in, stat_en);
   
   mux32 Mux32 (alu_result, 32'h0, wb_sel, write_data);
   
@@ -41,11 +41,11 @@ module sisc (clk, rst_f, ir);
 
   im InstructionMemory (pc_out, read_data);
 
-  ir InstructionRegister (clk, /*ir_load*/, read_data, /*instr*/);
+  ir InstructionRegister (clk, ir_load, read_data, instr);
 
   pc ProgramCounter (clk, br_addr, pc_sel, /*pc_write*/, /*pc_rst*/, pc_out);
 
-  br BranchCalculator (pc_out /*pc_in*/, ir[15:0] /*imm*/, br_sel, br_addr)
+  br BranchCalculator (pc_out /*pc_in*/, instr[15:0] /*imm*/, br_sel, br_addr)
   
                
   
